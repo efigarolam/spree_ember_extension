565985d80d27df8e9624432d52ef8f89fb75678a
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-26 12:39:00 -0600

    Requires needed gems

--- lib/spree_ember_extension.rb
+++ lib/spree_ember_extension.rb
@@ -1,6 +1,17 @@
-require "spree_ember_extension/version"
+require 'spree_ember_extension/version'
+require 'rails/all'
+require 'rails/engine'
+require 'ember-rails'
+require 'jquery-rails'
+require 'bootstrap-sass'
 
-module SpreeEmberExtension
-  class Engine < ::Rails::Engine
+module Spree
+  module Ember
+    module SpreeEmberExtension
+      class Engine < ::Rails::Engine
+        isolate_namespace Spree
+      end
+    end
   end
 end
+
